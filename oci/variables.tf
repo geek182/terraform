dc1183708c56b0ecd34ce990fef4d150968935a9
Leandro Azevedo <leandro.santos.azevedo@ing.com>
2022-03-02 19:01:51 +0100

    add policies to the tenancy

--- oci/variables.tf
+++ oci/variables.tf
@@ -33,6 +33,16 @@ variable "quota_statements" {
 		 "set compute quota bm-standard1-36-count to 0 in tenancy",
 		 "set compute quota bm-standard-b1-44-count to 0 in tenancy",
 		 "set compute quota bm-standard2-52-count to 0 in tenancy",
+		 "set compute quota bm-standard-e2-64-count	to 0 in tenancy",
+		 "set compute quota bm-dense-io1-36-count to 0 in tenancy",
+		 "set compute quota bm-dense-io2-52-count to 0 in tenancy",
+		 "set compute quota bm-gpu2-2-count to 0 in tenancy",
+		 "set compute quota bm-gpu3-8-count to 0 in tenancy",
+		 "set compute quota bm-hpc2-36-count to 0 in tenancy",
+		 "set compute quota vm-standard1-1-count to 0 in tenancy",
+		 "set compute quota vm-standard1-2-count to 0 in tenancy",
+		 "set compute quota vm-standard1-4-count to 0 in tenancy",
+		 "set compute quota vm-standard1-8-count to 0 in tenancy",
 	]
 }
 #variable "compartment_ocid" {
